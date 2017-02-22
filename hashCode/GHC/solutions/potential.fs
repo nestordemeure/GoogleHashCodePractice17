45718751aff179affadc131bb2482f0331a25420
Saetlan <s.arnaud67@hotmail.fr>
2017-02-23 00:02:46 +0100

    adding the fact that you generate bigger slices that minimal in potential

--- hashCode/GHC/solutions/potential.fs
+++ hashCode/GHC/solutions/potential.fs
@@ -29,21 +29,25 @@ let isLegalSlice (slice:Slice) (pizza:Pizza) (minIngr:int) (maxCells:int) =
 
 let potential (pizza:Pizza) minIngr maxCells =
     let sliceSet = MutableSet.empty
+    let minSlice = minIngr*2
+
     let mutable sliceLength = minIngr*2
     let mutable sliceHeight = 1
-    while sliceLength>=1 do
-        //printfn "%d" sliceLength
-        //System.Console.ReadLine() |> ignore
-        for j = 0 to ((Array2D.length1 pizza)-1) do
-            for i = 0 to ((Array2D.length2 pizza)-1) do
-                let sliceA = { left = i ; top = j-sliceHeight ; right = i+sliceLength; bottom = j; score=minIngr*2}
-                let sliceB = { left = i-sliceHeight ; top = j-sliceLength ; right = i; bottom = j; score=minIngr*2}
-                let sliceC = { left = i-sliceLength ; top = j ; right = i; bottom = i+sliceHeight; score=minIngr*2}
-                let sliceD = { left = i ; top = j ; right = i+sliceHeight; bottom = j+sliceLength; score=minIngr*2}
-                if (isLegalSlice sliceA pizza minIngr maxCells) then MutableSet.add sliceA sliceSet
-                if (isLegalSlice sliceB pizza minIngr maxCells) then MutableSet.add sliceB sliceSet
-                if (isLegalSlice sliceC pizza minIngr maxCells) then MutableSet.add sliceC sliceSet
-                if (isLegalSlice sliceD pizza minIngr maxCells) then MutableSet.add sliceD sliceSet
-        sliceLength<-sliceLength/2
-        if(sliceLength<>0) then sliceHeight<-(minIngr*2)/sliceLength
+    for z = minSlice to maxCells do
+        sliceLength<-z
+        while sliceLength>=1 do
+            //printfn "%d" sliceLength
+            //System.Console.ReadLine() |> ignore
+            for j = 0 to ((Array2D.length1 pizza)-1) do
+                for i = 0 to ((Array2D.length2 pizza)-1) do
+                    let sliceA = { left = i ; top = j-sliceHeight ; right = i+sliceLength; bottom = j; score=minIngr*2}
+                    let sliceB = { left = i-sliceHeight ; top = j-sliceLength ; right = i; bottom = j; score=minIngr*2}
+                    let sliceC = { left = i-sliceLength ; top = j ; right = i; bottom = i+sliceHeight; score=minIngr*2}
+                    let sliceD = { left = i ; top = j ; right = i+sliceHeight; bottom = j+sliceLength; score=minIngr*2}
+                    if (isLegalSlice sliceA pizza minIngr maxCells) then MutableSet.add sliceA sliceSet
+                    if (isLegalSlice sliceB pizza minIngr maxCells) then MutableSet.add sliceB sliceSet
+                    if (isLegalSlice sliceC pizza minIngr maxCells) then MutableSet.add sliceC sliceSet
+                    if (isLegalSlice sliceD pizza minIngr maxCells) then MutableSet.add sliceD sliceSet
+            sliceLength<-sliceLength/2
+            if(sliceLength<>0) then sliceHeight<-(minIngr*2)/sliceLength
     sliceSet
