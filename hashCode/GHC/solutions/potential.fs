08144c041ce22b81f73a676b7fdaa842a311fd9f
Yannis Weishaupt <yannis.weishaupt@gmail.com>
2017-02-22 21:33:53 +0100

    add isLegalSlice

--- hashCode/GHC/solutions/potential.fs
+++ hashCode/GHC/solutions/potential.fs
@@ -9,4 +9,12 @@ open GHC.Domain
 let potential (pizza:Pizza) minIngr maxCells =
    MutableSet.empty
 
-let is_legal_slice (slice:Slice) (minIngr:int) (maxCells:int) =
+let isLegalSlice (pizza:Pizza) (slice:Slice) (minIngr:int) (maxCells:int) =
+   let mutable nbMushroom = 0
+   let mutable nbTomato = 0
+   for x=slice.left to slice.right do
+      for y=slice.bottom to slice.top do
+         match pizza.[x, y] with
+         | M -> nbMushroom <- nbMushroom+1
+         | T -> nbTomato <- nbTomato+1
+   nbMushroom > minIngr && nbTomato > minIngr && nbTomato+nbMushroom < maxCells
