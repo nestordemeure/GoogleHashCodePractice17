efcaa16cf1eca2d245e82fc300444997bc220968
Yannis Weishaupt <yannis.weishaupt@gmail.com>
2017-02-22 22:05:09 +0100

    fix indent

--- hashCode/GHC/solutions/potential.fs
+++ hashCode/GHC/solutions/potential.fs
@@ -8,10 +8,10 @@ open GHC.Domain
 
 let potential (pizza:Pizza) minIngr maxCells =
     let mutable sliceLength = minIngr*2
-    while sliceLength>=1 do 
+    while sliceLength>=1 do
         for j = 1 to (Array2D.length2 pizza) do
             for i = 1 to (Array2D.length1 pizza) do
-        //MutableSet.add ({ left = 0 ; top = 0 ; right = 0; bottom = 0; score=0}) 
+        //MutableSet.add ({ left = 0 ; top = 0 ; right = 0; bottom = 0; score=0})
         sliceLength<-sliceLength/2
    //MutableSet.empty
 
@@ -19,8 +19,8 @@ let isLegalSlice (pizza:Pizza) (slice:Slice) (minIngr:int) (maxCells:int) =
    let mutable nbMushroom = 0
    let mutable nbTomato = 0
    for x=slice.left to slice.right do
-      for y=slice.bottom to slice.top do
-         match pizza.[x, y] with
-         | M -> nbMushroom <- nbMushroom+1
-         | T -> nbTomato <- nbTomato+1
+       for y=slice.bottom to slice.top do
+           match pizza.[x, y] with
+           | M -> nbMushroom <- nbMushroom+1
+           | T -> nbTomato <- nbTomato+1
    (nbMushroom >= minIngr) && (nbTomato >= minIngr) && (nbTomato+nbMushroom <= maxCells)
